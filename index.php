5eae6301517f7eb05ec62a7bc15cc017792b1691
unknown <psaenzguerrero@gmail.com>
2025-01-17 09:41:16 +0100

    un arregliyo tonto

--- index.php
+++ index.php
@@ -4,32 +4,36 @@ require_once 'Biblioteca.php';
 
 $biblioteca = new Biblioteca();
     
-$action = $_REQUEST['action'] ?? '';
+// $action = $_REQUEST['action'] ?? '';
 
-// Agregar libro
-if ($action === 'agregarLibro' && isset($_POST['titulo'], $_POST['autor'])) {
-    $biblioteca->agregarLibro($_POST['titulo'], $_POST['autor']);
-}
-// Borrar libro
-elseif ($action === 'borrar' && isset($_POST['id'])) {
-    $biblioteca->borrarLibro($_POST['id']);
-}
-// Actualizar estado de libro
-elseif ($action === 'actualizarEstado' && isset($_POST['id'], $_POST['estado'])) {
-    $biblioteca->actualizarEstado($_POST['id'], $_POST['estado']);
-}
-// Agregar autor
-elseif ($action === 'agregarAutor' && isset($_POST['nombre'])) {
-    $biblioteca->agregarAutor($_POST['nombre']);
-}
-// Borrar autor
-elseif ($action === 'borrarAutor' && isset($_POST['id'])) {
-    $biblioteca->borrarAutor($_POST['id']);
-}
-// Obtener libros y autores
-$libros = $biblioteca->obtenerLibros();
-$autores = $biblioteca->obtenerAutores();
+    if (isset($_REQUEST['action'])) {
+        $action = $_REQUEST['action'];
+        // Agregar libro
+        if ($action === 'agregarLibro' && isset($_POST['titulo'], $_POST['autor'])) {
+            $biblioteca->agregarLibro($_POST['titulo'], $_POST['autor']);
+        }
+        // Borrar libro
+        elseif ($action === 'borrar' && isset($_POST['id'])) {
+            $biblioteca->borrarLibro($_POST['id']);
+        }
+        // Actualizar estado de libro
+        elseif ($action === 'actualizarEstado' && isset($_POST['id'], $_POST['estado'])) {
+            $biblioteca->actualizarEstado($_POST['id'], $_POST['estado']);
+        }
+        // Agregar autor
+        elseif ($action === 'agregarAutor' && isset($_POST['nombre'])) {
+            $biblioteca->agregarAutor($_POST['nombre']);
+        }
+        // Borrar autor
+        elseif ($action === 'borrarAutor' && isset($_POST['id'])) {
+            $biblioteca->borrarAutor($_POST['id']);
+        }
+        // Obtener libros y autores
+        $libros = $biblioteca->obtenerLibros();
+        $autores = $biblioteca->obtenerAutores();
 
-// Incluir la vista de libros
-include 'libros.php';
+        // Incluir la vista de libros
+        include 'libros.php';
+    }
+
 ?>
